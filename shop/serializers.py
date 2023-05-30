9deaca7aa685a97d7b5914dbf7adf83af718ffad
azizvorrez <azizvorrez8@gmail.com>
2023-05-31 01:25:47 +0200

    Step 2

--- shop/serializers.py
+++ shop/serializers.py
@@ -4,4 +4,14 @@ from shop.models import *
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'date_created', 'date_updated']
+
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['date_created', 'date_updated', 'name', 'description', 'category']
+
+class ArticleSerializer(ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['date_created', 'date_updated', 'name', 'description', 'price']
--- shop/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-
-from shop import views
-
-urlpatterns = [
-    path('categories/', views.CategoryView.as_view())
-]
